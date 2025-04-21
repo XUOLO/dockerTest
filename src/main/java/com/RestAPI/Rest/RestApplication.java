a20ac8ff4089f1dbec0fa47c3f686ffe79366d30
loctx <>
2025-04-21 15:59:57 +0700

    commit

--- src/main/java/com/RestAPI/Rest/RestApplication.java
+++ src/main/java/com/RestAPI/Rest/RestApplication.java
@@ -2,12 +2,17 @@ package com.RestAPI.Rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
-
 }
